32e1bb279d10dcc124b4b4642260b591e0fe60ef
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 22:48:22 +0200

    Add project files.

---
+++ beeping/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace beeping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.Console.Beep();
+                textBox1.Text += "+";
+                Refresh();
+                Thread.Sleep(1000);
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
